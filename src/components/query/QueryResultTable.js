4f182ee476753c7903513b0025fd503b71f1be1e
avikt4 <avi.i@sprinto.com>
2023-11-28 05:17:49 +0530

    fix table messages alignment

--- src/components/query/QueryResultTable.js
+++ src/components/query/QueryResultTable.js
@@ -14,15 +14,17 @@ function QueryResultTable({ query }) {
       dataIndex: key,
     }));
   return (
-    <div className="max-h-[60vh] min-w-full overflow-auto rounded-lg mb-16 mt-4 flex-1 flex justify-center items-center absolute bottom-0 border-t-2">
-      {!error && !loading && !data && (
-        <span className="text-2xl opacity-40 flex flex-col items-center">
-          <CiViewTable size={45} />
-          Nothing to show here
-        </span>
-      )}
-      {error && "Error fetching query data"}
-      {loading && "loading..."}
+    <div className="h-[60vh] min-w-full overflow-auto rounded-lg mb-16 mt-4 flex-1 flex justify-center absolute bottom-0 border-t-2">
+      <div className="h-full w-full text-2xl opacity-40 flex flex-col items-center justify-center ">
+        {!error && !loading && !data && (
+          <>
+            <CiViewTable size={45} />
+            "Nothing to show here"
+          </>
+        )}
+        {error && "Error fetching query data"}
+        {loading && "loading..."}
+      </div>
       {!error && !loading && data && (
         <div>
           <Table
