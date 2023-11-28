39592e170b4031e7a271aecd9a8d3c90e1f6cf72
Avi Kumrawat <avikumrawat18@gmail.com>
2023-11-29 01:01:16 +0530

    add timeToFetch and dataRows  features

--- src/components/query/QueryResultTable.js
+++ src/components/query/QueryResultTable.js
@@ -1,18 +1,28 @@
-import React, { Suspense, useMemo } from 'react';
-import useFetchTableData from '../../hooks/useFetchTableData';
-import { CiViewTable } from 'react-icons/ci';
+import React, { Suspense, useMemo } from "react";
+import useFetchTableData from "../../hooks/useFetchTableData";
+import { CiViewTable } from "react-icons/ci";
 
-const Table = React.lazy(() => import('antd').then(module => ({ default: module.Table })));
+const Table = React.lazy(() =>
+  import("antd").then((module) => ({ default: module.Table }))
+);
 
 function QueryResultTable({ query }) {
-  const { data, loading, error } = useFetchTableData(query);
+  const {
+    data: { data, dataRows },
+    loading,
+    error,
+    timeToFetch,
+  } = useFetchTableData(query);
 
-  const columns = useMemo(() => 
-    data ? Object.keys(data[0]).map(key => ({
-      key,
-      title: key === 'key' ? '#' : key,
-      dataIndex: key,
-    })) : [],
+  const columns = useMemo(
+    () =>
+      data
+        ? Object.keys(data[0]).map((key) => ({
+            key,
+            title: key === "key" ? "#" : key,
+            dataIndex: key,
+          }))
+        : [],
     [data]
   );
 
@@ -38,12 +48,18 @@ function QueryResultTable({ query }) {
   return (
     <div className="h-[60vh] min-w-full overflow-auto rounded-lg mb-16 mt-4 flex-1 flex justify-center absolute bottom-0 border-t-2">
       <Suspense fallback={<div>Loading Table...</div>}>
-        <Table
-          dataSource={data}
-          columns={columns}
-          pagination={{ pageSize: 10 }}
-          scroll={{ y: 430 }}
-        />
+        <div className="flex flex-col">
+          <div className="flex opacity-50 italic p-4 gap-4">
+            <p>Fetched data in {timeToFetch}s</p>
+            <p>{dataRows} rows</p>
+          </div>
+          <Table
+            dataSource={data}
+            columns={columns}
+            pagination={{ pageSize: 10 }}
+            scroll={{ y: 430 }}
+          />
+        </div>
       </Suspense>
     </div>
   );
--- src/hooks/useFetchTableData.js
+++ src/hooks/useFetchTableData.js
@@ -3,55 +3,64 @@ import axios from "axios";
 import { parseCSVData } from "../utils";
 
 const useFetchTableData = (tableName) => {
-  console.log("fetching data", tableName);
   const [data, setData] = useState(null);
+  const [dataLength, setDataLength] = useState(0);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-
-  // Using an object to cache the responses.
+  const [timeToFetch, setTimeToFetch] = useState(null);
   const cache = useRef({}).current;
 
   const fetchData = useCallback(async () => {
     if (!tableName) {
       setData(null);
       setError(null);
+      setTimeToFetch(null);
+      setDataLength(0);
       return;
     }
 
-    // Reset data and error states on new fetch
     setData(null);
     setError(null);
+    setTimeToFetch(null);
+    setDataLength(0);
 
-    // Check if data for this table is already cached.
     if (cache[tableName]) {
-      setData(cache[tableName].raw);
+      setData(cache[tableName].data.data);
+      setTimeToFetch(cache[tableName].timeToFetch);
+      setDataLength(cache[tableName].data.dataRows);
       return;
     }
 
     setLoading(true);
+    const startTime = performance.now();
     try {
       const response = await axios.get(
         `https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/${tableName}.csv`
       );
-      cache[tableName] = { raw: response.data }; // Cache the raw response data
-      setData(response.data);
+      const endTime = performance.now();
+      let fetchTime = endTime - startTime;
+      fetchTime = (fetchTime/1000).toFixed(2)   //convert to seconds rounded off to 2 decimal places
+      const processedData = parseCSVData(response.data);
+      const length = processedData.length;
+      cache[tableName] = {
+        data: { data: processedData, dataRows: length },
+        timeToFetch: fetchTime,
+      };
+      setData(processedData);
+      setTimeToFetch(fetchTime);
+      setDataLength(length);
     } catch (err) {
       setError(err);
     } finally {
       setLoading(false);
     }
-  }, [tableName]); // eslint-disable-line
+  }, [tableName]);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-  // Memoize processed data
-  const processedData = useMemo(() => {
-    return data ? parseCSVData(data) : null;
-  }, [data]);
-
-  return { data: processedData, loading, error };
+  return { data: { data, dataRows: dataLength }, loading, error, timeToFetch };
 };
 
 export default useFetchTableData;
