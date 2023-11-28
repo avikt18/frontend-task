9c913be47fb8b9c28135d447ed75770a03e57cea
Avi Kumrawat <avikumrawat18@gmail.com>
2023-11-28 07:03:40 +0530

    optimize QueryTable using lazy loading

--- src/components/query/QueryResultTable.js
+++ src/components/query/QueryResultTable.js
@@ -1,40 +1,50 @@
-import React from "react";
-import { Table } from "antd";
-import useFetchTableData from "../../hooks/useFetchTableData";
-import { CiViewTable } from "react-icons/ci";
+import React, { Suspense, useMemo } from 'react';
+import useFetchTableData from '../../hooks/useFetchTableData';
+import { CiViewTable } from 'react-icons/ci';
+
+const Table = React.lazy(() => import('antd').then(module => ({ default: module.Table })));
 
 function QueryResultTable({ query }) {
   const { data, loading, error } = useFetchTableData(query);
 
-  let columns =
-    data &&
-    Object.keys(data[0]).map((key) => ({
-      key: key,
-      title: key === "key" ? "#" : key,
+  const columns = useMemo(() => 
+    data ? Object.keys(data[0]).map(key => ({
+      key,
+      title: key === 'key' ? '#' : key,
       dataIndex: key,
-    }));
+    })) : [],
+    [data]
+  );
+
+  if (error) {
+    return <div>Error fetching query data</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data) {
+    return (
+      <div className="h-[60vh] min-w-full overflow-auto rounded-lg mb-16 mt-4 flex-1 flex justify-center absolute bottom-0 border-t-2">
+        <div className="h-full w-full text-2xl opacity-40 flex flex-col items-center justify-center">
+          <CiViewTable size={45} />
+          "Nothing to show here"
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="h-[60vh] min-w-full overflow-auto rounded-lg mb-16 mt-4 flex-1 flex justify-center absolute bottom-0 border-t-2">
-      <div className="h-full w-full text-2xl opacity-40 flex flex-col items-center justify-center ">
-        {!error && !loading && !data && (
-          <>
-            <CiViewTable size={45} />
-            "Nothing to show here"
-          </>
-        )}
-        {error && "Error fetching query data"}
-        {loading && "loading..."}
-      </div>
-      {!error && !loading && data && (
-        <div>
-          <Table
-            dataSource={data}
-            columns={columns}
-            pagination={{ pageSize: 10 }}
-            scroll={{ y: 430 }}
-          />
-        </div>
-      )}
+      <Suspense fallback={<div>Loading Table...</div>}>
+        <Table
+          dataSource={data}
+          columns={columns}
+          pagination={{ pageSize: 10 }}
+          scroll={{ y: 430 }}
+        />
+      </Suspense>
     </div>
   );
 }
