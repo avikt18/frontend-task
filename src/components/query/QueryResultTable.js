2298a7bcc33809d256359d07c5c2266c7f4d56e7
Avi Kumrawat <avikumrawat18@gmail.com>
2023-11-29 01:30:21 +0530

    fix loading and error states alignment

--- src/components/query/QueryResultTable.js
+++ src/components/query/QueryResultTable.js
@@ -25,29 +25,44 @@ function QueryResultTable({ query }) {
         : [],
     [data]
   );
-
   if (error) {
-    return <div>Error fetching query data</div>;
+    return (
+      <QueryResultTableWrapper>
+        <div className="h-full w-full text-2xl opacity-40 flex flex-col items-center justify-center">
+          Error fetching query data
+        </div>
+      </QueryResultTableWrapper>
+    );
   }
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <QueryResultTableWrapper>
+        <div className="h-full w-full text-2xl opacity-40 flex flex-col items-center justify-center">
+          Loading...
+        </div>
+      </QueryResultTableWrapper>
+    );
   }
 
   if (!data) {
     return (
-      <div className="h-[60vh] min-w-full overflow-auto rounded-lg mb-16 mt-4 flex-1 flex justify-center absolute bottom-0 border-t-2">
+      <QueryResultTableWrapper>
         <div className="h-full w-full text-2xl opacity-40 flex flex-col items-center justify-center">
           <CiViewTable size={45} />
           Nothing to show here
         </div>
-      </div>
+      </QueryResultTableWrapper>
     );
   }
 
   return (
-    <div className="h-[60vh] min-w-full overflow-auto rounded-lg mb-16 mt-4 flex-1 flex justify-center absolute bottom-0 border-t-2">
-      <Suspense fallback={<div>Loading Table...</div>}>
+    <QueryResultTableWrapper>
+      <Suspense
+        fallback={
+          <QueryResultTableWrapper>Loading Table...</QueryResultTableWrapper>
+        }
+      >
         <div className="flex flex-col">
           <div className="flex opacity-50 italic p-4 gap-4">
             <p>Fetched data in {timeToFetch}s</p>
@@ -61,8 +76,14 @@ function QueryResultTable({ query }) {
           />
         </div>
       </Suspense>
-    </div>
+    </QueryResultTableWrapper>
   );
 }
 
 export default QueryResultTable;
+
+const QueryResultTableWrapper = ({ children }) => (
+  <div className="h-[60vh] min-w-full overflow-auto rounded-lg mb-16 mt-4 flex-1 flex justify-center absolute bottom-0 border-t-2">
+    {children}
+  </div>
+);
