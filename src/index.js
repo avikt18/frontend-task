9a5cc2c9b6787db5c44a5afa6838a49cc0d516a4
Avi Kumrawat <avikumrawat18@gmail.com>
2023-11-28 06:28:44 +0530

    downgrade react version ti fix deployment issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
 import "./index.css";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import App from './App';
+
+const rootElement = document.getElementById('root');
+render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	rootElement
+);
